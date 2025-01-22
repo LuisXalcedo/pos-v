729b69f96bce37df3b5ff81dc243b28ebc15e51d
LuisXalcedo <luis.e.ss@gmail.com>
2025-01-21 18:42:40 -0700

    Update NavAnalytics component to use Pathnames type for href property

--- pos/src/components/nav-analytics.tsx
+++ pos/src/components/nav-analytics.tsx
@@ -37,7 +37,7 @@ export function NavAnalytics({
             <SidebarMenuButton asChild>
               <Link
                 key={item.title}
-                href={item.href as string}
+                href={item.href as Pathnames}
                 className={clsx(
                   "flex w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
                   {
